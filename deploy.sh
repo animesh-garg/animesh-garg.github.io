79d05b1a1034801066423fffb06cde74ca220e32
Animesh Garg <garg.animesh@gmail.com>
2020-03-08 21:33:33 -0400

    adding assets: images and pdfs

--- deploy.sh
+++ deploy.sh
@@ -4,26 +4,34 @@
 
 #delete _site
 bundle exec jekyll clean 
+echo "--------build folder: _site cleaned------------"
 
 # clone master in _site
 git clone -b master https://github.com/animesh-garg/animesh-garg.github.io.git ./_site
-
+echo "--------Master cloned------------"
 
 # build into _site folder 
 bundle exec jekyll build
+echo "--------Site built------------"
 
 # move to site 
 cd _site
 
-# initiate a new repo and add all files in _site 
+# git status
+git status
+echo "--------Site Diffs------------"
+
 # add new commmit
 git add .
 now=$(date)
 git commit -m "site updated at $now"
+echo "--------Commits staged------------"
+echo "commit message: site updated at $now"
 
 git push
+echo "--------Commits pushed------------"
 
 # clean up
 cd ..
 bundle exec jekyll clean 
-
+echo "--------build folder: _site cleaned------------"
