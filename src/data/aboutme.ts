c629640ec53c8b990c9f9e18140cb144d999a90e
Animesh Garg <animesh-garg@users.noreply.github.com>
2025-06-18 09:49:41 -0400

    fix broken links

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -27,16 +27,15 @@ export const aboutMe: AboutMe = {
   institution: "Georgia Tech",
   // Note that links work in the description
   description: `
-    I am a <a href=https://research.gatech.edu/animesh-garg>Stephen Fleming Early Career Professor</a> in Computer Science at Georgia Tech. I am in the <a href=https://www.ic.gatech.edu/person/faculty><i>School of Interactive Computing</i></a> affiliated with <a href=https://www.research.gatech.edu/robotics/faculty/core><i>Robotics</i></a> and <a href=https://ml.gatech.edu/people/faculty/phdprogramfaculty><i>Machine Learning</i></a> programs. I also hold courtesy appointments at University of Toronto and Vector Institute. I have previously held research leadership positions at Nvidia and Apptronik. <br><br>
+    I am a <a href=https://research.gatech.edu/animesh-garg>Stephen Fleming Early Career Professor</a> in Computer Science at Georgia Tech. I am in the <a href=https://www.ic.gatech.edu><i>School of Interactive Computing</i></a> affiliated with <a href=https://www.research.gatech.edu/robotics/faculty/core><i>Robotics</i></a> and <a href=https://ml.gatech.edu/people/faculty/phdprogramfaculty><i>Machine Learning</i></a> programs. I also hold courtesy appointments at University of Toronto and Vector Institute. I have previously held research leadership positions at Nvidia and Apptronik. <br><br>
     My research vision is to build the <i>Algorithmic Foundations for Generalizable Autonomy</i>, that enables robots to acquire skills, at both cognitive & dexterous levels, and to seamlessly interact & collaborate with humans in novel environments. My group focuses on understanding <i>structured inductive biases</i> and <i>causality for decision making</i>. In particular we are looking at <i>multi-modal object-centric and spatiotemporal event representations</i>, <i>self-supervised pre-training for reinforcement learning & control</i>, <i>principle of efficient dexterous skill learning</i>.<br><br>
     <b>Research Interests:</b> Foundation Models, Reinforcement Learning, Robotics, 3D Vision.<br>
-    <b>Current Applications:</b> Self-Driving Labs, Surgical Robotics, Personal Robotics.<br><br>
-    <i>Check out <a href="https://pairlab.github.io/"><b>PAIR Group</b></a> for info on our research and how to join us.</i>
+    <b>Current Applications:</b> Self-Driving Labs, Surgical Robotics, Manufacturing.<br><br>
+    <i>Check out <a href="https://pairlab.github.io"><b>PAIR Group</b></a> for info on our research and how to join us.</i>
     `,
   email: "animesh.garg@gatech.edu",
   imageUrl:    
     "/assets/animesh-garg-profile-white-nov23.jpg",
-    // "/assets/animesh-profile-nov23.jpg",
   googleScholarUrl: "https://scholar.google.com/citations?user=zp8V7ZMAAAAJ",
   githubUsername: "animesh-garg",
   linkedinUsername: "animeshgarg",
@@ -44,7 +43,7 @@ export const aboutMe: AboutMe = {
   blogUrl: "https://roboticks.substack.com/",
   cvUrl: "/assets/garg-cv-0425.pdf",
   institutionUrl: "https://www.gatech.edu",
-  labPage: "www.pair.toronto.edu",
+  labPage: "https://www.pair.toronto.edu",
   addressOffice: "CODA S1145, Atlanta, GA",
   address: `
   CODA Building S1145,  <br> 
--- src/data/teaching.ts
+++ src/data/teaching.ts
@@ -13,21 +13,21 @@ export const teachingData: Teaching[] = [
     title: "Introduction to AI (UG)",
     offering: "Sp25",
     // description: "Large GT class on intro to ai",
-    link: "https://www.cc.gatech.edu/classes/AY2025/cs3600_fall/ ",
+    link: "",
   },
   {
     number: "CS 8803",
     title: "Deep Reinforcement Learning (Grad)",
     offering: "F24, F25",
     // description: "Graduate class on deep reinforcement learning",
-    link: "",
+    link: "https://www.pair.toronto.edu/cs8803-drl-f24/",
   },
   {
     number: "CSC 375",
     title: "Algorithmic Intelligence in Robotics (UG)",
     offering: "F20, Sp22",
     // description: "Introduction to AI in robotic manipulation",
-    link: "",
+    link: "https://www.pair.toronto.edu/csc375-f20/",
   },
   {
     number: "CSC 2547",
@@ -39,10 +39,12 @@ export const teachingData: Teaching[] = [
     number: "CSC 475",
     title: "Introduction to Reinforcement Learning (UG) ",
     offering: "Sp21, F21",
+    link:"https://www.pair.toronto.edu/csc498-f21/",
   },  
   {
     number: "CSC 2621",
     title: "Reinforcement Learning (Grad)",
     offering: "Sp20",
+    link:"https://www.pair.toronto.edu/csc2621-w20/",
   }
 ];
